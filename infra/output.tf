e49c6af590fc685aae5a9d0c0e46ddaf64a7409d
krasov <me@frostfree.ru>
2025-09-20 20:52:47 +0300

    add tests

--- infra/output.tf
+++ infra/output.tf
@@ -1,10 +1,20 @@
 
-output "vm-prod-ip" {
+output "vm_dev_address" {
+  description = "The external IP address of the development VM"
+  value       = yandex_compute_instance.vm-dev.network_interface.0.nat_ip_address
+}
+
+output "vm_dev_name" {
+  description = "The name of the development VM"
+  value       = yandex_compute_instance.vm-dev.name
+}
+
+output "vm_prod_address" {
   description = "The external IP address of the production VM"
   value       = yandex_compute_instance.vm-prod.network_interface.0.nat_ip_address
 }
 
-output "vm-dev-ip" {
-  description = "The external IP address of the development VM"
-  value       = yandex_compute_instance.vm-dev.network_interface.0.nat_ip_address
+output "vm_prod_name" {
+  description = "The name of the production VM"
+  value       = yandex_compute_instance.vm-prod.name
 }
